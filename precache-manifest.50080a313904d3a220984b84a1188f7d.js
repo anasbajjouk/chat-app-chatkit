88aff610d65c4c2e399da181a8cb47dd7c340fb7
Anas Bajjouk <bajjouk1@gmail.com>
2019-12-25 23:17:14 +0100

    Updates

---
+++ precache-manifest.50080a313904d3a220984b84a1188f7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e62280ae01dcb63bd4208255c2410cd",
+    "url": "/chatkit/index.html"
+  },
+  {
+    "revision": "2d9045594d54d8faab3e",
+    "url": "/chatkit/static/css/main.a5b42d94.chunk.css"
+  },
+  {
+    "revision": "a3f044cdd781972bfac8",
+    "url": "/chatkit/static/js/2.0b2e65a4.chunk.js"
+  },
+  {
+    "revision": "2d9045594d54d8faab3e",
+    "url": "/chatkit/static/js/main.d03829f3.chunk.js"
+  },
+  {
+    "revision": "8e3a7dd8ceb1bab62c4a",
+    "url": "/chatkit/static/js/runtime-main.26f49140.js"
+  }
+]);
